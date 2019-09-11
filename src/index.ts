be3a83f3f88f37cdac0790d027d758d3c838f98d
ナッティ <hello@nutty.cc>
2019-09-10 20:13:10 -0700

    Configure webpack and TypeScript

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const wrapper = document.getElementById('wrapper')
+const test = document.createElement('p')
+test.innerHTML = 'TypeScript works!'
+wrapper.appendChild(test)
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = (env) => {
+  config = {
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: [
+        '.tsx',
+        '.ts',
+        '.js'
+      ]
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    }
+  }
+
+  if (env.production) {
+    return Object.assign(config, {
+      mode: 'production',
+      optimization: {
+        minimizer: [new UglifyJsPlugin()]
+      }
+    })
+  } else {
+    return Object.assign(config, {
+      mode: 'development',
+      devtool: 'inline-source-map'
+    })
+  }
+}
