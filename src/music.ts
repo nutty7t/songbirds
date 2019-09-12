6aa3ee6d0ace18908c549e1596b88b20860b2af5
ナッティ <hello@nutty.cc>
2019-09-12 14:40:06 -0700

    Generate major and minor chord progressions

--- src/music.ts
+++ src/music.ts
@@ -58,26 +58,51 @@ function reducer (notes: Record<number, Note>, midiNumber: number): Record<numbe
 const midiNumbers: Array<number> = Array.from({ length: 128 }, (_, key) => key)
 const notes: Record<number, Note> = midiNumbers.reduce(reducer, {})
 
-const stochasticMatrices = {
-  major: [
-    /*           I     ii    iii   IV    V     vi    vii°
-    /* I    */  [0.00, 0.10, 0.01, 0.20, 0.60, 0.04, 0.05],
-    /* ii   */  [0.00, 0.00, 0.00, 0.00, 0.75, 0.00, 0.25],
-    /* iii  */  [0.00, 0.00, 0.00, 0.60, 0.00, 0.40, 0.00],
-    /* IV   */  [0.20, 0.20, 0.00, 0.00, 0.40, 0.00, 0.20],
-    /* V    */  [0.80, 0.00, 0.00, 0.00, 0.00, 0.20, 0.00],
-    /* VI   */  [0.00, 0.50, 0.00, 0.50, 0.00, 0.00, 0.00],
-    /* vii° */  [1.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
-  ],
-  minor: [
-    /*           i     ii°   III   iv    V     VI    vii°  VII
-    /* i    */  [0.00, 0.20, 0.01, 0.20, 0.40, 0.09, 0.05, 0.05],
-    /* ii°  */  [0.00, 0.00, 0.00, 0.00, 0.90, 0.00, 0.00, 0.10],
-    /* III  */  [0.00, 0.20, 0.00, 0.40, 0.00, 0.40, 0.00, 0.00],
-    /* iv   */  [0.20, 0.15, 0.00, 0.00, 0.50, 0.00, 0.15, 0.00],
-    /* V    */  [0.80, 0.00, 0.00, 0.00, 0.00, 0.00, 0.20, 0.00],
-    /* VI   */  [0.00, 0.60, 0.00, 0.40, 0.00, 0.00, 0.00, 0.00],
-    /* vii° */  [1.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-    /* VII  */  [0.00, 0.00, 1.00, 0.00, 0.00, 0.00, 0.00, 0.00]
-  ]
+function generateChordProgression(mode: Mode, n: number): Array<number> {
+  // These stochastic matrices are generated from the chord diagrams
+  // in the Tonal Harmony (Kostka, Payne) textbook and assigning arbitrary
+  // probabilities to each edge in the graph.
+  const stochasticMatrices = {
+    [Mode.Major]: [
+      /*           I     ii    iii   IV    V     vi    vii°
+      /* I    */  [0.00, 0.10, 0.01, 0.20, 0.60, 0.04, 0.05],
+      /* ii   */  [0.00, 0.00, 0.00, 0.00, 0.75, 0.00, 0.25],
+      /* iii  */  [0.00, 0.00, 0.00, 0.60, 0.00, 0.40, 0.00],
+      /* IV   */  [0.20, 0.20, 0.00, 0.00, 0.40, 0.00, 0.20],
+      /* V    */  [0.80, 0.00, 0.00, 0.00, 0.00, 0.20, 0.00],
+      /* VI   */  [0.00, 0.50, 0.00, 0.50, 0.00, 0.00, 0.00],
+      /* vii° */  [1.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
+    ],
+    [Mode.Minor]: [
+      /*           i     ii°   III   iv    V     VI    vii°  VII
+      /* i    */  [0.00, 0.20, 0.01, 0.20, 0.40, 0.09, 0.05, 0.05],
+      /* ii°  */  [0.00, 0.00, 0.00, 0.00, 0.90, 0.00, 0.00, 0.10],
+      /* III  */  [0.00, 0.20, 0.00, 0.40, 0.00, 0.40, 0.00, 0.00],
+      /* iv   */  [0.20, 0.15, 0.00, 0.00, 0.50, 0.00, 0.15, 0.00],
+      /* V    */  [0.80, 0.00, 0.00, 0.00, 0.00, 0.00, 0.20, 0.00],
+      /* VI   */  [0.00, 0.60, 0.00, 0.40, 0.00, 0.00, 0.00, 0.00],
+      /* vii° */  [1.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
+      /* VII  */  [0.00, 0.00, 1.00, 0.00, 0.00, 0.00, 0.00, 0.00]
+    ]
+  }
+
+  let currentChord: number = 0
+  let chordProgression: Array<number> = [currentChord]
+  let matrix = stochasticMatrices[mode]
+
+  // Step through the Markov chain.
+  for (let c = 1; c < length; c++) {
+    const selection = Math.random()
+    let probabilityBound = 0.00
+    for (let nc = 0; nc < matrix[currentChord].length; nc++) {
+      probabilityBound += matrix[currentChord][nc]
+      if (selection < probabilityBound) {
+        chordProgression.push(nc)
+        currentChord = nc
+        break
+      }
+    }
+  }
+
+  return chordProgression
 }
